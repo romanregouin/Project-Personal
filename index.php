fe7073f3dc3c6c5bf5bbb5a8bfc6fb48ed37f86f
Roman <romanregouin@live.fr>
2020-02-02 18:47:18 +0100

    Fichier copié

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Let's have fun !</title>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    </head>
+    <body>
+        <?php include("menu.php"); ?>
+        <?php include("contenu.php"); ?>
+    </body>
+    <?php include("footer.php"); ?>
+</html>
