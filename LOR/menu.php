ceaf07157fc6eae7962ec324c403b722312cb793
Roman <romanregouin@live.fr>
2020-02-06 17:24:44 +0100

    CSS barre menu

--- LOR/menu.php
+++ LOR/menu.php
@@ -1,4 +1,5 @@
 <ul id="menu">
     <li class="menu_item"><a class="text_menu" href="region.php">Region</a></li>
+    <li class="menu_item"><img id="menu_icon" src="Img/menu_icon.png" alt="menu_icon"></li>   
     <li class="menu_item"><a class="text_menu" href="rarity.php">Rarity</a></li>
 </ul>
